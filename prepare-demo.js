1882ba536c2644959f58a3499e4620f92e0baa61
krishpranav <krisna.pranav@gmail.com>
2021-05-26 16:22:21 +0530

    [+] added some demo functions [+]

--- prepare-demo.js
+++ prepare-demo.js
@@ -10,3 +10,17 @@ if (fs.existsSync("./demo/lib")) {
     fs.rmdirSync("./demo/lib", { recursive: true });
 }
   
+fse.copySync("./dist", "./demo/dist");
+fse.copySync("./node_modules/semver-umd", "./demo/lib/semver-umd");
+fse.copySync("./node_modules/vscode-oniguruma", "./demo/lib/vscode-oniguruma");
+fse.copySync("./node_modules/vscode-textmate", "./demo/lib/vscode-textmate");
+
+if(fs.existsSync('./demo/dist/extensions/vscode-web-playground')){
+    fs.rmdirSync('./demo/dist/extensions/vscode-web-playground', { recursive: true })
+}
+child_process.execSync('git clone https://github.com/microsoft/vscode-web-playground.git  demo/dist/extensions/vscode-web-playground', {stdio: 'inherit'});
+process.chdir('demo/dist/extensions/vscode-web-playground');
+child_process.execSync('yarn', {stdio: 'inherit'});
+child_process.execSync('yarn compile', {stdio: 'inherit'});
+
+process.chdir('../../../..');
