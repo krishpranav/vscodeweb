9ce0778a1fc0dcd4fcd4db9f41fd5b1cb679076e
krishpranav <krisna.pranav@gmail.com>
2021-05-26 16:40:12 +0530

    [+] Added window product functions [+]

--- workbench.ts
+++ workbench.ts
@@ -1,3 +1,14 @@
 import { create, IWorkbenchConstructionOptions, IWorkspaceProvider, IWorkspace } from 'vs/workbench/workbench.web.api';
 import { URI, UriComponents } from 'vs/base/common/uri';
 declare const window: any;
+
+(async function() {
+    let config: IWorkbenchConstructionOptions & { folderUri?: UriComponents, workspaceUri?: UriComponents } = {};
+
+    if (window.product) {
+        config = window.product
+    } else {
+        const result = await fetch('/product.json')
+        config = await result.json();
+    }
+})
