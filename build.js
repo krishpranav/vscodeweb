c275264033a331861bc61ec1c2627f2680d47d80
krishpranav <krisna.pranav@gmail.com>
2021-05-26 16:44:04 +0530

    [+] const vaules added [+]

---
+++ build.js
@@ -0,0 +1,9 @@
+// const values
+const process = require("process");
+const child_process = require("child_process");
+const fs = require("fs");
+const fse = require("fs-extra");
+const glob = require("glob");
+const rmdir = require('rimraf');
+
+const vscodeVersion = "1.56.0"
