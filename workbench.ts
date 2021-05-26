a8684aea2a6927db05c6c55c38a27e868fa01c1b
krishpranav <krisna.pranav@gmail.com>
2021-05-26 16:37:23 +0530

    [+] added imports, declared variable [+]

---
+++ workbench.ts
@@ -0,0 +1,3 @@
+import { create, IWorkbenchConstructionOptions, IWorkspaceProvider, IWorkspace } from 'vs/workbench/workbench.web.api';
+import { URI, UriComponents } from 'vs/base/common/uri';
+declare const window: any;
